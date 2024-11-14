2206011392be63dd0d7b7646baa6a2726e04965f
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2024-11-13 21:18:10 -0300

    feat: add benefits component

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -5,37 +5,39 @@
 import * as $$$$$$$$$$$0 from "./apps/deco/blog.ts";
 import * as $$$$$$$$$$$1 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$2 from "./apps/site.ts";
-import * as $$$$$$0 from "./sections/BlogPosts.tsx";
-import * as $$$$$$1 from "./sections/Faq.tsx";
-import * as $$$$$$2 from "./sections/Features.tsx";
-import * as $$$$$$3 from "./sections/Footer.tsx";
-import * as $$$$$$4 from "./sections/Header.tsx";
-import * as $$$$$$11 from "./sections/hero-1.tsx";
-import * as $$$$$$12 from "./sections/hero-2.tsx";
-import * as $$$$$$5 from "./sections/Hero.tsx";
-import * as $$$$$$13 from "./sections/hero2.tsx";
-import * as $$$$$$6 from "./sections/ImageWithDescription.tsx";
-import * as $$$$$$7 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$8 from "./sections/Logos.tsx";
-import * as $$$$$$9 from "./sections/Testimonials.tsx";
-import * as $$$$$$10 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$0 from "./sections/Benefits.tsx";
+import * as $$$$$$1 from "./sections/BlogPosts.tsx";
+import * as $$$$$$2 from "./sections/Faq.tsx";
+import * as $$$$$$3 from "./sections/Features.tsx";
+import * as $$$$$$4 from "./sections/Footer.tsx";
+import * as $$$$$$5 from "./sections/Header.tsx";
+import * as $$$$$$12 from "./sections/hero-1.tsx";
+import * as $$$$$$13 from "./sections/hero-2.tsx";
+import * as $$$$$$6 from "./sections/Hero.tsx";
+import * as $$$$$$14 from "./sections/hero2.tsx";
+import * as $$$$$$7 from "./sections/ImageWithDescription.tsx";
+import * as $$$$$$8 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$9 from "./sections/Logos.tsx";
+import * as $$$$$$10 from "./sections/Testimonials.tsx";
+import * as $$$$$$11 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
-    "site/sections/BlogPosts.tsx": $$$$$$0,
-    "site/sections/Faq.tsx": $$$$$$1,
-    "site/sections/Features.tsx": $$$$$$2,
-    "site/sections/Footer.tsx": $$$$$$3,
-    "site/sections/Header.tsx": $$$$$$4,
-    "site/sections/hero-1.tsx": $$$$$$11,
-    "site/sections/hero-2.tsx": $$$$$$12,
-    "site/sections/Hero.tsx": $$$$$$5,
-    "site/sections/hero2.tsx": $$$$$$13,
-    "site/sections/ImageWithDescription.tsx": $$$$$$6,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$7,
-    "site/sections/Logos.tsx": $$$$$$8,
-    "site/sections/Testimonials.tsx": $$$$$$9,
-    "site/sections/Theme/Theme.tsx": $$$$$$10,
+    "site/sections/Benefits.tsx": $$$$$$0,
+    "site/sections/BlogPosts.tsx": $$$$$$1,
+    "site/sections/Faq.tsx": $$$$$$2,
+    "site/sections/Features.tsx": $$$$$$3,
+    "site/sections/Footer.tsx": $$$$$$4,
+    "site/sections/Header.tsx": $$$$$$5,
+    "site/sections/hero-1.tsx": $$$$$$12,
+    "site/sections/hero-2.tsx": $$$$$$13,
+    "site/sections/Hero.tsx": $$$$$$6,
+    "site/sections/hero2.tsx": $$$$$$14,
+    "site/sections/ImageWithDescription.tsx": $$$$$$7,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$8,
+    "site/sections/Logos.tsx": $$$$$$9,
+    "site/sections/Testimonials.tsx": $$$$$$10,
+    "site/sections/Theme/Theme.tsx": $$$$$$11,
   },
   "apps": {
     "site/apps/deco/blog.ts": $$$$$$$$$$$0,
